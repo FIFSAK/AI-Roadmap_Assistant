2ec9b7db19f34af2f5eb5eafa15687d48ef9414d
FIFSAK <anuar200572@gmail.com>
2023-07-19 00:17:36 +0600

    effect typing work in test page

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,19 +1,23 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Page = () => {
     const [ws, setWs] = useState(null);
     const [responses, setResponses] = useState([]);
+    const lastResponse = useRef(''); // This is a ref to store the last received response
 
     useEffect(() => {
         const websocket = new WebSocket("ws://localhost:8000/ws");
         websocket.onopen = () => setWs(websocket);
-        websocket.onmessage = (event) => setResponses(old => [...old, event.data]);
         websocket.onclose = (event) => console.log('WebSocket closed', event);
         websocket.onerror = (error) => console.log('WebSocket error', error);
         websocket.onmessage = (event) => {
             console.log('WebSocket message', event.data);
-            setResponses(old => [...old, event.data]);
+            
+            const newPart = event.data.slice(lastResponse.current.length);
+            lastResponse.current = event.data; // Update the lastResponse ref
+            
+            setResponses(old => [...old, newPart]);
         };
         return () => websocket.close();
     }, []);
@@ -21,6 +25,8 @@ const Page = () => {
     const sendText = (text) => {
         if (ws) {
             ws.send(text);
+            setResponses([]); // Reset the responses state whenever a new text is sent
+            lastResponse.current = ''; // Also reset the lastResponse ref
         }
     };
 
@@ -33,3 +39,41 @@ const Page = () => {
 };
 
 export default Page;
+
+
+// 'use client'
+// import { useEffect, useState } from "react";
+
+// const Page = () => {
+//     const [ws, setWs] = useState(null);
+//     const [responses, setResponses] = useState([]);
+
+//     useEffect(() => {
+//         const websocket = new WebSocket("ws://localhost:8000/ws");
+//         websocket.onopen = () => setWs(websocket);
+//         websocket.onclose = (event) => console.log('WebSocket closed', event);
+//         websocket.onerror = (error) => console.log('WebSocket error', error);
+//         websocket.onmessage = (event) => {
+//             console.log('WebSocket message', event.data);
+//             setResponses([event.data]);
+//         };
+//         return () => websocket.close();
+//     }, []);
+
+//     const sendText = (text) => {
+//         if (ws) {
+//             ws.send(text);
+//             setResponses([]); // Reset the responses state whenever a new text is sent
+//         }
+//     };
+
+//     return (
+//         <div>
+//             {responses.map((response, index) => <p key={index}>{response}</p>)}
+//             <button onClick={() => sendText("president of kazakhstan?")}>Send Text</button>
+//         </div>
+//     );
+// };
+
+// export default Page;
+
