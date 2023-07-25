726566933fe95b6f80540f93da53956840d2e5b8
FIFSAK <anuar200572@gmail.com>
2023-07-25 13:29:26 +0600

    replace name roadmap on its first str

--- app/my_roadmaps/page.jsx
+++ app/my_roadmaps/page.jsx
@@ -62,45 +62,51 @@ const useRoadmaps = () => {
   };
   
 
-const UserRoadmaps = () => {
-  const { roadmaps, deleteRoadmap } = useRoadmaps();
-  const [shownRoadmapIndex, setShownRoadmapIndex] = useState(null);
-
-  if (roadmaps === null) {
-    return <Loading />;
-  }
-
-  if (!roadmaps || roadmaps.length === 0) {
-    return <p>No roadmaps found</p>;
-  }
-
-  const formatRoadmapText = (text) => {
-    const formattedText = text.replace(/\n/g, '<br />')
-      .replace(/(http:\/\/|https:\/\/|www\.)[\w.-]+(\.[\w.-]+)+([\w.,@?^=%&:;\/~+#-]*[\w@?^=%&\/~+#-])?/g, '<a href="$&" target="_blank" rel="noopener noreferrer">$&</a>');
-    return { __html: formattedText };
-  };
-
-  const showRoadmap = (index) => {
-    setShownRoadmapIndex(index === shownRoadmapIndex ? null : index);
-  };
-
-  return (
-    <div style={{ padding: "50px", backgroundColor: "#f3f3f3", minHeight: "100vh" }}>
-      <h1 style={{ textAlign: "center", color: "#4f4f4f", marginBottom: "50px" }}>User Roadmaps</h1>
-      {roadmaps.map((roadmap, index) => (
-        <div style={cardStyle} key={index}>
-          <div style={titleRowStyle}>
-            <h2 style={titleStyle} onClick={() => showRoadmap(index)}>
-              Roadmap {index + 1}
-            </h2>
-            <button style={deleteButtonStyle} onClick={() => deleteRoadmap(index)}>Delete</button>
+  const UserRoadmaps = () => {
+    const { roadmaps, deleteRoadmap } = useRoadmaps();
+    const [shownRoadmapIndex, setShownRoadmapIndex] = useState(null);
+  
+    if (roadmaps === null) {
+      return <Loading />;
+    }
+  
+    if (!roadmaps || roadmaps.length === 0) {
+      return <p>No roadmaps found</p>;
+    }
+  
+    const formatRoadmapText = (text) => {
+      const formattedText = text.replace(/\n/g, '<br />')
+        .replace(/(http:\/\/|https:\/\/|www\.)[\w.-]+(\.[\w.-]+)+([\w.,@?^=%&:;\/~+#-]*[\w@?^=%&\/~+#-])?/g, '<a href="$&" target="_blank" rel="noopener noreferrer">$&</a>');
+      return { __html: formattedText };
+    };
+  
+    const getFirstLine = (text) => {
+      const lines = text.split('\n');
+      const firstLine = lines[0] || 'Roadmap';
+      return firstLine.length > 61 ? firstLine.slice(0, 61) + '...' : firstLine;
+    };
+  
+    const showRoadmap = (index) => {
+      setShownRoadmapIndex(index === shownRoadmapIndex ? null : index);
+    };
+  
+    return (
+      <div style={{ padding: "50px", backgroundColor: "#f3f3f3", minHeight: "100vh" }}>
+        <h1 style={{ textAlign: "center", color: "#4f4f4f", marginBottom: "50px" }}>User Roadmaps</h1>
+        {roadmaps.map((roadmap, index) => (
+          <div style={cardStyle} key={index}>
+            <div style={titleRowStyle}>
+              <h2 style={titleStyle} onClick={() => showRoadmap(index)}>
+                {getFirstLine(roadmap)}
+              </h2>
+              <button style={deleteButtonStyle} onClick={() => deleteRoadmap(index)}>Delete</button>
+            </div>
+            {shownRoadmapIndex === index && <p style={contentStyle}>{parse(formatRoadmapText(roadmap).__html)}</p>}
           </div>
-          {shownRoadmapIndex === index && <p style={contentStyle}>{parse(formatRoadmapText(roadmap).__html)}</p>}
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  };
 
 const cardStyle = {
   backgroundColor: "white",
