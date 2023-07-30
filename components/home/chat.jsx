93714240f6034e385d385e9870b356865343aebb
FIFSAK <anuar200572@gmail.com>
2023-07-30 22:33:41 +0600

    add intro card

--- components/home/chat.jsx
+++ components/home/chat.jsx
@@ -5,12 +5,12 @@ import { useEffect, useRef, useState } from 'react';
 import './blur.css';
 import { ChatLine } from './chat-line';
 
-const initialMessages = [
-  {
-    role: 'assistant',
-    content: "Hello there! I'm your Personal IT Roadmap Assistant.I'm here to help you navigate the exciting world of IT. I can make a roadmap for beginners or based on your existing experience. if you have not decided on a profession, go to the page list of majors or take a survey",
-  },
-]
+// const initialMessages = [
+//   {
+//     role: 'assistant',
+//     content: "Hello there! I'm your Personal IT Roadmap Assistant.I'm here to help you navigate the exciting world of IT. I can make a roadmap for beginners or based on your existing experience. if you have not decided on a profession, go to the page list of majors or take a survey",
+//   },
+// ]
 
 const InputMessage = ({ input, setInput, sendMessage }) => {
   const inputRef = useRef(null)
@@ -55,8 +55,9 @@ const InputMessage = ({ input, setInput, sendMessage }) => {
 const useMessages = () => {
   const [messages, setMessages] = useState(() => {
     const storedMessages = localStorage.getItem('chatMessages');
-    return storedMessages ? JSON.parse(storedMessages) : initialMessages;
+    return storedMessages ? JSON.parse(storedMessages) : [];
   });
+  const [isRoadmapCreated, setIsRoadmapCreated] = useState(false);
   const [ws, setWs] = useState(null);
   const [wsLinks, setWsLinks] = useState(null);
   const [links, setLinks] = useState('');
@@ -124,6 +125,7 @@ const useMessages = () => {
     try {
       if (ws) {
         ws.send(newMessage);
+        setIsRoadmapCreated(true);
       }
     } catch (err) {
       console.error(err);
@@ -159,6 +161,7 @@ useEffect(() => {
   if (links) {
     setMessages(old => [...old.slice(0, -1), { role: 'assistant', content: messages[messages.length - 1].content + "\n" + links }]);
     setLinks('');
+    setIsRoadmapCreated(false);
   }
 }, [links]);
   
@@ -170,37 +173,57 @@ useEffect(() => {
     messages,
     sendMessage,
     likeMessage,
-    handleGetLinks
+    handleGetLinks,
+    isRoadmapCreated
   }
 }
 
 
 export default function Chat() {
   const [input, setInput] = useState('')
-  const { messages, sendMessage, likeMessage, handleGetLinks } = useMessages()
+  const { messages, sendMessage, likeMessage, handleGetLinks, isRoadmapCreated } = useMessages()
   const messagesEndRef = useRef(null)
+  const [isInstructionVisible, setIsInstructionVisible] = useState(true);
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
   }, [messages])
 
+  const handleSendMessage = (message) => {
+    setIsInstructionVisible(false);
+    sendMessage(message);
+  }
+
   return (
     <div className="flex-1 w-full border-zinc-100 overflow-hidden">
+      {isInstructionVisible && (
+        <div className="flex flex-col items-center justify-start h-screen pt-40">
+          <div className="p-5 bg-white rounded shadow-xl max-w-2xl w-full">
+            <h2 className="text-2xl font-bold mb-4 text-center text-black">Instructions for the bot:</h2>
+            <p className="mb-4 text-black text-lg">- Write an instruction here</p>
+            <h2 className="text-2xl font-bold mb-4 text-center text-black">Some examples on how to use this bot:</h2>
+            <p className="mb-4 text-black text-lg">- Write an example here</p>
+          </div>
+        </div>
+      )}
       <div style={{ maxHeight: 'calc(100vh - 165px)', overflowY: 'scroll' }}>
         {messages.map(({ content, role }, index) => (
           <ChatLine key={index} role={role} content={content} onLike={() => likeMessage(content)} />
         ))}
-        <button
-          className="mx-auto flex w-fit items-center gap-3 rounded border border-neutral-200 bg-white py-2 px-4 text-black text-sm hover:opacity-50 disabled:opacity-25"
-          onClick={handleGetLinks}
-        >
-          <div className="w-4 h-4">
-            <AcademicCapIcon />
-          </div> {'Get links'}
-        </button>
+        {isRoadmapCreated && (
+          <button
+            className="mx-auto flex w-fit items-center gap-3 rounded border border-neutral-200 bg-white py-2 px-4 text-black text-sm hover:opacity-50 disabled:opacity-25"
+            onClick={handleGetLinks}
+          >
+            <div className="w-4 h-4">
+              <AcademicCapIcon />
+            </div> {'Get links'}
+          </button>
+        )}
         <div ref={messagesEndRef} />
       </div>
-      <InputMessage input={input} setInput={setInput} sendMessage={sendMessage} />
+      <InputMessage input={input} setInput={setInput} sendMessage={handleSendMessage} />
     </div>
   )
 }
+
